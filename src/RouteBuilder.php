489a55822d34b6f6495dae0e8dd82b537119c2fb
Seth Battis <sbattis@groton.org>
2025-09-18 21:15:41 -0400

    feat: inject inner middleware into RouteBuilder

--- src/RouteBuilder.php
+++ src/RouteBuilder.php
@@ -10,19 +10,26 @@ use GrotonSchool\Slim\LTI\Actions\LoginAction;
 use GrotonSchool\Slim\LTI\Actions\RegistrationStartAction;
 use GrotonSchool\Slim\LTI\Middleware\CookieMiddleware;
 use GrotonSchool\Slim\Norms\RouteBuilderInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface;
 use Slim\Interfaces\RouteGroupInterface;
 
 class RouteBuilder implements RouteBuilderInterface
 {
-    public function define(App $app): RouteGroupInterface
+    public function define(App $app, ?MiddlewareInterface ...$innerMiddleware): RouteGroupInterface
     {
-        return $app->group('/lti', function (RouteCollectorProxyInterface $lti) {
+        $group = $app->group('/lti', function (RouteCollectorProxyInterface $lti) {
             $lti->post('/launch', LaunchAction::class);
             $lti->get('/jwks', JWKSAction::class);
             $lti->get('/register', RegistrationStartAction::class);
             $lti->post('/login', LoginAction::class);
-        })->add(CookieMiddleware::class);
+        });
+
+        foreach ($innerMiddleware as $middleware) {
+            $group = $group->add($middleware);
+        }
+
+        return $group->add(CookieMiddleware::class);
     }
 }
